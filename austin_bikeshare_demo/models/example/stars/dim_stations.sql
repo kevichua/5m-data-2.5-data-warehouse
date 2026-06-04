d893f5333633f14f69689bcfb266a95a5403076a
kevichua <kevichua@gmail.com>
2026-06-04 23:25:01 +0800

    commit

--- austin_bikeshare_demo/models/example/stars/dim_stations.sql
+++ austin_bikeshare_demo/models/example/stars/dim_stations.sql
@@ -1,19 +1,29 @@
-SELECT DISTINCT
-    station_id,
-    name,
-    status,
-    location,
-    address,
-    alternate_name,
-    city_asset_number,
-    property_type,
-    number_of_docks,
-    power_type,
-    footprint_length,
-    footprint_width,
-    notes,
-    council_district,
-    image,
-    modified_date
-FROM {{ source('austin_bikeshare', 'bikeshare_stations') }}
+WITH station_base AS (
+    SELECT
+        station_id,
+        name AS station_name,
+        status,
+        address,
+    FROM {{ source('austin_bikeshare', 'bikeshare_stations') }}
+),
+
+trip_starts AS (
+    SELECT
+        CAST(start_station_id AS INT64) AS station_id,
+        AVG(duration_minutes * 60) AS avg_duration
+    FROM {{ source('austin_bikeshare', 'bikeshare_trips') }}
+    WHERE start_station_id IS NOT NULL
+    GROUP BY start_station_id
+)
+
+SELECT
+    sb.station_id,
+    sb.station_name,
+    sb.status,
+    sb.address,
+    ts.avg_duration
+FROM station_base sb
+LEFT JOIN trip_starts ts
+    ON sb.station_id = ts.station_id
+ORDER BY avg_duration DESC
   
